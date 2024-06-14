beaaaf9dc83c257d6e0504926a129ef793f03597
Revilo1512 <oliver.klager@stud.fh-campuswien.ac.at>
2024-06-14 16:40:53 +0200

    can create use and login/logout via the homepage

--- swiftbook/api/urls.py
+++ swiftbook/api/urls.py
@@ -2,11 +2,14 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('login/', views.loginUserView, name ='Login'),
-    path('logout/', views.logoutUserView, name ='Logout'),
-    path('register/', views.registerUserView, name ='Register'),
+    path('login/', views.loginUserView, name ='login'),
+    path('logout/', views.logoutUserView, name ='logout'),
+    path('register/', views.registerUserView, name ='register'),
+    
+    path('user-calendar/', views.userCalendarView, name='User_Calender'),
 
 
 
-    path('user-calendar/', views.userCalendarView, name='User_Calender')
+    # Added new path for homepage
+    path('', views.homepage, name='homepage'),
 ]
