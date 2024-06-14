1bfb276721e351187e54c8f9f20172c2d2e6d10a
Martn <martin.schoenfeld@gmx.at>
2024-06-14 19:51:18 +0200

    add session to adminpanel

--- swiftbook/api/admin.py
+++ swiftbook/api/admin.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
 from .models import Provider, Service, Booking, BusinessHours
+from django.contrib.sessions.models import Session
+
 
 # Register your models here.
 admin.site.register(Provider)
 admin.site.register(Service)
 admin.site.register(Booking)
 admin.site.register(BusinessHours)
+admin.site.register(Session)
 
--- swiftbook/api/models.py
+++ swiftbook/api/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.contrib.auth.models import User
 
+
 class Provider(models.Model): 
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
